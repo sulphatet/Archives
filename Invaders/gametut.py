a8f6c9c3993854cad9444800f9d1b1720a239b39
Syed Affan D <73064995+sulphatet@users.noreply.github.com>
2022-05-19 17:08:39 +0530

    Update gametut.py

--- Invaders/gametut.py
+++ Invaders/gametut.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 import random
 import math
 
@@ -17,18 +18,22 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('alien.png')
 playerX = 370
 playerY = 480
+print('WELCOME TO INVADERS: ')
+print('SPACEBAR to shoot, KEYS to move your ship')
+print('Make sure the invaders do not attack the spaceship! If they come too close, YOU LOSE ')
+print('Watch out! As time passes, your visibility will be affected.')
 
 enemyImg = []
 enemyX = []
 enemyY = []
 enemyChangeX = []
-EnemyNum = 2
+EnemyNum = 5
 
 for i in range(EnemyNum):
     enemyImg.append(pygame.image.load('space-ship.png'))
     enemyX.append(random.randint(0,735))
     enemyY.append(random.randint(50,150))
-    enemyChangeX.append(0.2)
+    enemyChangeX.append(0.3)
     
 bulletImg = pygame.image.load('bullet.png')
 bulletX = 480
@@ -38,6 +43,9 @@ bulletChangeY = 0.80
 bullet_state = "ready"
 
 score = 0
+
+flag = 0
+over_font = pygame.font.Font('freesansbold.ttf',64)
 
 def player(playerX,playerY):
     screen.blit(playerImg,(playerX,playerY))
@@ -58,12 +66,15 @@ def isCollision(enemyX,enemyY,bulletX,bulletY):
         return True
     return False
 
+color = 200
 playerChangeX = 0
 #game loop
 running = True
-while running:
+z = 1
+x = time.time()
+while running: 
     #print(pygame.event.get())
-    screen.fill((200,200,200))
+    screen.fill((color,color,color))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -82,21 +93,44 @@ while running:
             playerChangeX = 0 
             
     for i in range(EnemyNum):
+        if enemyY[i] > 450:
+            for j in range(EnemyNum):
+                enemyY[j] = 2000
+            print("LOST, GAME OVER")
+            quit()
+        
         enemyX[i] += enemyChangeX[i]
         if enemyX[i] <= 0:
-            enemyChangeX[i] = 0.3
+            enemyChangeX[i] = 0.3*z
         if enemyX[i] >= 736:
-            enemyChangeX[i] = -0.3
-            enemyY[i] += 25
+            enemyChangeX[i] = -0.3*z
+            enemyY[i] += 29
         collision = isCollision(enemyX[i],enemyY[i],bulletX,bulletY)
         if collision:
             bulletY = 480
             bullet_state = 'ready'
             score += 3
-            print(score)
+            print('Score: ' + str(score))
             enemyX[i] = random.randint(0,735)
             enemyY[i] = random.randint(50,150)
         enemy(enemyX[i],enemyY[i],i)
+        
+    y = time.time()
+    
+    if(int(y-x) > 5):
+        flag +=1
+        z += 0.1
+        x = y
+    
+    if flag == 3:
+        if color > 50:
+            color -= 50
+            for i in range(EnemyNum):
+                transparency = 128
+                enemyImg[i].fill((255,255,255,transparency),special_flags = pygame.BLEND_RGBA_MULT)
+        flag = 0
+
+                
     if playerX <= 0:
         playerX = 0
     if playerX >= 736:
@@ -116,4 +150,4 @@ while running:
 
     player(playerX,playerY)
     pygame.display.update()
-    
+ 
